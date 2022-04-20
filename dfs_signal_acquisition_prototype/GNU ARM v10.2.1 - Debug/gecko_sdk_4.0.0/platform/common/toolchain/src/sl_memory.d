2cb193334875671f1204545dcd7fd39a9ed67bc9
ShaheerRana <s32rana@uwaterloo.ca>
2022-04-20 13:28:57 -0400

    Changed project name

---
+++ dfs_signal_acquisition_prototype/GNU ARM v10.2.1 - Debug/gecko_sdk_4.0.0/platform/common/toolchain/src/sl_memory.d
@@ -0,0 +1,10 @@
+gecko_sdk_4.0.0/platform/common/toolchain/src/sl_memory.o: \
+ ../gecko_sdk_4.0.0/platform/common/toolchain/src/sl_memory.c \
+ C\:\Users\Co-op\ Shaheeer\ R\SimplicityStudio\dfs\dfs_signal_acquisition_prototype\gecko_sdk_4.0.0\platform\common\toolchain\inc/sl_gcc_preinclude.h \
+ C\:\Users\Co-op\ Shaheeer\ R\SimplicityStudio\dfs\dfs_signal_acquisition_prototype\gecko_sdk_4.0.0\platform\common\toolchain\inc/sl_memory.h \
+ C\:\Users\Co-op\ Shaheeer\ R\SimplicityStudio\dfs\dfs_signal_acquisition_prototype\gecko_sdk_4.0.0\platform\common\toolchain\inc/sl_memory_region.h \
+ C\:\Users\Co-op\ Shaheeer\ R\SimplicityStudio\dfs\dfs_signal_acquisition_prototype\config/sl_memory_config.h
+C\:\Users\Co-op\ Shaheeer\ R\SimplicityStudio\dfs\dfs_signal_acquisition_prototype\gecko_sdk_4.0.0\platform\common\toolchain\inc/sl_gcc_preinclude.h:
+C\:\Users\Co-op\ Shaheeer\ R\SimplicityStudio\dfs\dfs_signal_acquisition_prototype\gecko_sdk_4.0.0\platform\common\toolchain\inc/sl_memory.h:
+C\:\Users\Co-op\ Shaheeer\ R\SimplicityStudio\dfs\dfs_signal_acquisition_prototype\gecko_sdk_4.0.0\platform\common\toolchain\inc/sl_memory_region.h:
+C\:\Users\Co-op\ Shaheeer\ R\SimplicityStudio\dfs\dfs_signal_acquisition_prototype\config/sl_memory_config.h:
---
+++ dfs_signal_acquisition_prototype/GNU ARM v10.2.1 - Debug/gecko_sdk_4.0.0/platform/emlib/src/em_assert.d
@@ -0,0 +1,6 @@
+gecko_sdk_4.0.0/platform/emlib/src/em_assert.o: \
+ ../gecko_sdk_4.0.0/platform/emlib/src/em_assert.c \
+ C\:\Users\Co-op\ Shaheeer\ R\SimplicityStudio\dfs\dfs_signal_acquisition_prototype\gecko_sdk_4.0.0\platform\common\toolchain\inc/sl_gcc_preinclude.h \
+ C\:\Users\Co-op\ Shaheeer\ R\SimplicityStudio\dfs\dfs_signal_acquisition_prototype\gecko_sdk_4.0.0\platform\emlib\inc/em_assert.h
+C\:\Users\Co-op\ Shaheeer\ R\SimplicityStudio\dfs\dfs_signal_acquisition_prototype\gecko_sdk_4.0.0\platform\common\toolchain\inc/sl_gcc_preinclude.h:
+C\:\Users\Co-op\ Shaheeer\ R\SimplicityStudio\dfs\dfs_signal_acquisition_prototype\gecko_sdk_4.0.0\platform\emlib\inc/em_assert.h:
