817790f71ed428fc64e61d545403ffc0e8c3e265
ShaheerRana <s32rana@uwaterloo.ca>
2022-04-20 10:25:51 -0400

    Cleaned up repo

--- BRD4259A_EFR32FG14P_adc_single_prs_gpio_dma - Copy/GNU ARM v10.2.1 - debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"C:/SiliconLabs/SimplicityStudio/v5/developer/toolchains/gnu_arm/10.2_2020q4//arm-none-eabi/include" -lgcc -lc -lm -lnosys
-
